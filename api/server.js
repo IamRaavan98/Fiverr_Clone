dc1d1e7f1e8abe9f8bbd7342fac9c37368e609e7
RohanOfficeGit <rohanagrawal1798@gmail.com>
2023-08-06 14:51:19 +0530

    some more changes

--- api/server.js
+++ api/server.js
@@ -33,7 +33,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({}));
+// app.use(cors({}));
 
 // Convert import.meta.url to a filesystem path
 const __filename = fileURLToPath(import.meta.url);
